07d9f1efb102da2b6c398f509c0350e2ac2d93e2
Jerry <szy0syz@gmail.com>
2020-08-17 09:41:41 +0800

    Initial client && bootstrap

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import "./App.scss";
+
+function App() {
+  return (
+    <Container className="pt-5">
+      <Row className="bg-white py-5 justify-content-center">
+        <Col sm={8} md={6} lg={4}>
+          <h1 className="text-center">Register</h1>
+          <Form>
+            <Form.Group>
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Confirm password</Form.Label>
+              <Form.Control type="password" />
+            </Form.Group>
+
+            <div className="text-center">
+              <Button variant="success" type="submit">
+                Register
+              </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
